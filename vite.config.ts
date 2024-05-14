39bb33a9e89a961350838044b5cfc60505e21dd3
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-05-14 23:01:43 +0800

    优化 vite 配置，调整代码分片策略并规范资源文件命名

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +7,36 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          echarts: ["echarts", "echarts-for-react"],
-          "roku-ui": ["roku-ui"],
+        manualChunks: (id: string) => {
+          if (id.includes('node_modules')) {
+            const packageName = id.toString().split('node_modules/');
+            const name = packageName[2].split('/')[0].toString();
+            if (name === 'react' || name === 'react-dom') {
+              return 'react';
+            }
+            if (name === 'echarts') {
+              return 'echarts';
+            }
+            return 'vendor';
+          }
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          if (!assetInfo.name) return '';
+          if (
+            assetInfo.type === 'asset' &&
+            /\.(jpe?g|png|gif|svg)$/i.test(assetInfo.name)
+          ) {
+            return 'assets/img/[name].[hash].[ext]';
+          }
+          if (
+            assetInfo.type === 'asset' &&
+            /\.(ttf|woff|woff2|eot)$/i.test(assetInfo.name)
+          ) {
+            return 'assets/fonts/[name].[hash].[ext]';
+          }
+          return 'assets/[ext]/[name]-[hash].[ext]';
         },
       },
     },
