9213b75a89088041fea0661ae1bc8f8dcbfcb343
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-05-15 00:30:05 +0800

    feat: 优化构建配置，简化代码并提高性能

--- vite.config.ts
+++ vite.config.ts
@@ -7,36 +7,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: (id: string) => {
-          if (id.includes('node_modules')) {
-            const packageName = id.toString().split('node_modules/');
-            const name = packageName[2].split('/')[0].toString();
-            if (name === 'react' || name === 'react-dom') {
-              return 'react';
-            }
-            if (name === 'echarts') {
-              return 'echarts';
-            }
-            return 'vendor';
-          }
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          if (!assetInfo.name) return '';
-          if (
-            assetInfo.type === 'asset' &&
-            /\.(jpe?g|png|gif|svg)$/i.test(assetInfo.name)
-          ) {
-            return 'assets/img/[name].[hash].[ext]';
-          }
-          if (
-            assetInfo.type === 'asset' &&
-            /\.(ttf|woff|woff2|eot)$/i.test(assetInfo.name)
-          ) {
-            return 'assets/fonts/[name].[hash].[ext]';
-          }
-          return 'assets/[ext]/[name]-[hash].[ext]';
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          echarts: ['echarts'],
         },
       },
     },
