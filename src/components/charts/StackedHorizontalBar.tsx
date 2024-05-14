f643e00d4ecb23eab0375066d60b5888b16733f7
zzwtsy <58852496+zzwtsy@users.noreply.github.com>
2024-05-14 23:00:53 +0800

    style：优化了代码的格式，修复了一些小错误。

--- src/components/charts/StackedHorizontalBar.tsx
+++ src/components/charts/StackedHorizontalBar.tsx
@@ -1,5 +1,5 @@
-import Big from "big.js";
-import { BarSeriesOption, LineSeriesOption } from "echarts/charts";
+import Big from 'big.js';
+import { BarSeriesOption, LineSeriesOption } from 'echarts/charts';
 import {
   DataZoomComponentOption,
   GridComponentOption,
@@ -7,17 +7,15 @@ import {
   TitleComponentOption,
   ToolboxComponentOption,
   TooltipComponentOption,
-} from "echarts/components";
-import {
-  CallbackDataParams,
-  ComposeOption,
-  TopLevelFormatterParams,
-} from "echarts/types/dist/shared";
-import { renderToString } from "react-dom/server";
-import { Panel } from "roku-ui";
-import { timeConverter } from "../../utils";
-import Chart from "./Chart";
-import { BarSeriesDataType } from "../../types";
+} from 'echarts/components';
+import { renderToString } from 'react-dom/server';
+import { Panel } from 'roku-ui';
+import { timeConverter } from '../../utils';
+import Chart from './Chart';
+import { BarSeriesDataType } from '../../types';
+import { ComposeOption } from 'echarts/core';
+import { TopLevelFormatterParams } from 'echarts/types/dist/shared.js';
+import { CallbackDataParams } from 'echarts/types/dist/shared.js';
 
 type EChartsOption = ComposeOption<
   | TooltipComponentOption
@@ -45,7 +43,7 @@ export default function StackedHorizontalChart({
     <Panel
       border
       className="flex rounded-lg overflow-hidden"
-      style={{ minHeight: "350px", width: "100%" }}
+      style={{ minHeight: '350px', width: '100%' }}
     >
       <Chart
         option={getOption(seriesData, xAxisData, title)}
@@ -62,12 +60,12 @@ function getOption(
 ): EChartsOption {
   return {
     title: {
-      left: "center",
+      left: 'center',
       text: title,
     },
     dataZoom: [
       {
-        type: "slider",
+        type: 'slider',
       },
     ],
     toolbox: {
@@ -79,7 +77,7 @@ function getOption(
       },
     },
     tooltip: {
-      trigger: "axis",
+      trigger: 'axis',
       formatter: (param: TopLevelFormatterParams) => {
         const params = param as CallbackDataParams[];
         const tooltipItems = TooltipItems(params);
@@ -87,7 +85,7 @@ function getOption(
           <>
             <span>{params[0].name}</span>
             <br />
-            {tooltipItems.length === 0 ? "There is nothing here" : tooltipItems}
+            {tooltipItems.length === 0 ? 'There is nothing here' : tooltipItems}
           </>
         );
 
@@ -95,16 +93,16 @@ function getOption(
       },
     },
     legend: {
-      type: "scroll",
-      top: "8%",
+      type: 'scroll',
+      top: '8%',
     },
     grid: {
-      left: "4%",
-      right: "6%",
+      left: '4%',
+      right: '6%',
       containLabel: true,
     },
     yAxis: {
-      type: "value",
+      type: 'value',
       axisLabel: {
         formatter: (value) => {
           return `${Big(value).div(3600).toFixed(0)} h`;
@@ -112,7 +110,7 @@ function getOption(
       },
     },
     xAxis: {
-      type: "category",
+      type: 'category',
       data: xAxisData,
     },
     series: barSeriesData,
@@ -121,51 +119,51 @@ function getOption(
 
 function TooltipItems(params: CallbackDataParams[]) {
   return params
-    .filter(({ value }) => typeof value === "number" && Math.floor(value) > 0)
+    .filter(({ value }) => typeof value === 'number' && Math.floor(value) > 0)
     .map(({ color, seriesName, value, seriesId }) => {
       return (
         <div
           key={seriesId}
           style={{
-            margin: "0 0 0",
+            margin: '0 0 0',
             lineHeight: 1,
           }}
         >
-          <div style={{ margin: "0 0 0", lineHeight: 1 }}>
+          <div style={{ margin: '0 0 0', lineHeight: 1 }}>
             <span
               style={{
-                display: "inline-block",
-                marginRight: "4px",
-                borderRadius: "10px",
-                width: "10px",
-                height: "10px",
-                backgroundColor: color === undefined ? "" : color.toString(),
+                display: 'inline-block',
+                marginRight: '4px',
+                borderRadius: '10px',
+                width: '10px',
+                height: '10px',
+                backgroundColor: color === undefined ? '' : color.toString(),
               }}
             ></span>
             <span
               style={{
-                fontSize: "14px",
-                color: "#666",
+                fontSize: '14px',
+                color: '#666',
                 fontWeight: 400,
-                marginLeft: "2px",
+                marginLeft: '2px',
               }}
             >
               {seriesName}
             </span>
             <span
               style={{
-                float: "right",
-                marginLeft: "20px",
-                fontSize: "14px",
-                color: "#666",
+                float: 'right',
+                marginLeft: '20px',
+                fontSize: '14px',
+                color: '#666',
                 fontWeight: 900,
               }}
             >
-              {timeConverter(typeof value === "number" ? value : 0)}
+              {timeConverter(typeof value === 'number' ? value : 0)}
             </span>
-            <div style={{ clear: "both" }}></div>
+            <div style={{ clear: 'both' }}></div>
           </div>
-          <div style={{ clear: "both" }}></div>
+          <div style={{ clear: 'both' }}></div>
         </div>
       );
     });
